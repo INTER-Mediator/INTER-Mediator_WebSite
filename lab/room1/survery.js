76d8073a2e1c4fc7be236e74292f94c6b2d40aac
msyk <nii@msyk.net>
2015-02-13 20:45:07 +0900

    Left just the files for this site.

--- lab/room1/survery.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by msyk on 2013/10/16.
- */
-
-function dateTimeForSQL(dt) {
-    return dt.getFullYear() + "-" + (dt.getMonth() + 1) + "-" + dt.getDate()
-        + " " + dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds()
-}
