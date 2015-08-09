a6c3d2e90ae16c0a1e344c8bf1e301b9d0e3a281
Masayuki Nii <msyk@msyk.net>
2015-08-09 18:46:24 +0900

    Survey for Study mtg at NII

---
+++ lab/room5/survey.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: msyk
+ * Date: 15/04/24
+ * Time: 23:42
+ */
+require_once('INTER-Mediator/INTER-Mediator.php');
+
+IM_Entry(
+    array(
+        array(
+            'name' => 'survey',
+            'key' => 'id',
+            'post-reconstruct' => true,
+            'post-dismiss-message' => 'ありがとうございました',
+            'validation' => array(
+                array(
+                    'field' => 'name',
+                    'rule' => "value != ''",
+                    'message' => '何か入力してください',
+                    'notify' => 'inline'
+                ),
+            ),
+        ),
+        array(
+            'name' => 'surveylist',
+            'view' => 'survey',
+            'table' => 'survey',
+            'key' => 'id',
+            'records' => 10,
+            'maxrecords' => 10,
+            'paging' => true,
+            'repeat-control' => 'confirm-delete',
+            'query' => array(
+                //      array('field' => 'Q2', 'operator'=>'>', 'value' => '43')
+            )
+        ),
+    ),
+    null,
+    array('db-class' => 'PDO'),
+    false
+);
